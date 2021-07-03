6f3a7efb0366ee3370ec38c727b98ccdcb0d870d
loveblue0121 <loveblue0121@gmail.com>
2021-07-03 19:40:31 +0800

    add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css'
-import Home from './pages/Home'
-import * as Icon from 'react-feather' // icon
+import Header from './pages/Home/HomeHeader/Header'
+//import * as Icon from 'react-feather' // icon
 
 function App() {
   return (
     <>
-      <Home />
+      <Header />
     </>
   )
 }
