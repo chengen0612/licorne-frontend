8fef1b13632550a662d896031a8bca62434f92c1
chengen0612 <79037530+chengen0612@users.noreply.github.com>
2021-07-04 15:59:54 +0800

    dev: finish HomeMap visual design also add search feat

--- src/pages/Home/HomeMap/MapCard.js
+++ src/pages/Home/HomeMap/MapCard.js
@@ -1,12 +1,33 @@
+import { useState, useRef } from 'react'
 import * as Icon from 'react-feather' // icon
 import MapListItem from './MapListItem'
+import shop_list from './shop_list.json'
 
 export default function MapCard() {
+  const [shops, setShops] = useState([
+    {
+      course_place_name: '台北店',
+      course_place_address: '台北市松山區龍人北路七段55號',
+      course_place_phone: '02-58851234',
+    },
+  ])
+
+  const queryString = useRef(null)
+
+  const queryHandler = () => {
+    const keyword = queryString.current.value
+    if (keyword.length === 0) return
+    const results = shop_list.filter((item) => {
+      return item.course_place_address.includes(keyword)
+    })
+    setShops(results)
+  }
+
   return (
     <>
       <div className="map__card">
         <div className="map__top">
-          <h4>實體店鋪</h4>
+          <h2 className="map__top-title">實體店鋪</h2>
           <div className="map__search-bar">
             <figure className="map__map-icon-top">
               <Icon.MapPin className="feather-s" />
@@ -15,14 +36,25 @@ export default function MapCard() {
               type="text"
               placeholder="縣市、鄉鎮"
               className="map__input"
+              ref={queryString}
+              onKeyPress={(event) => event.key === 'Enter' && queryHandler()}
             />
-            <figure className="map__search-icon-top">
+            <figure className="map__search-icon-top" onClick={queryHandler}>
               <Icon.Search className="map__search-icon feather-m" />
             </figure>
           </div>
         </div>
         <div className="map__content">
-          <MapListItem />
+          {shops.map((shop, i) => {
+            return (
+              <MapListItem
+                key={i}
+                name={shop.course_place_name}
+                address={shop.course_place_address}
+                phone={shop.course_place_phone}
+              />
+            )
+          })}
         </div>
       </div>
     </>
--- src/pages/Home/HomeMap/MapListItem.js
+++ src/pages/Home/HomeMap/MapListItem.js
@@ -1,14 +1,20 @@
-export default function MapListItem() {
+export default function MapListItem({ name, address, phone }) {
+  const routeLinkHandler = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <>
       <div className="map__item">
-        <h5 className="map__item-title">高雄小港旗艦店</h5>
+        <h5 className="map__item-title">{name}</h5>
         <span className="map__item-intro">
-          地址：812高雄市小港區中山街617號 <br />
-          電話：0613583297 <br />
+          地址：{address} <br />
+          電話：{phone} <br />
           營業時間：週二至週日 10:00 - 20:00
         </span>
-        <span className="map__route-link">前往路線</span>
+        <a href="/" className="map__route-link" onClick={routeLinkHandler}>
+          前往路線
+        </a>
         <button className="map__store-btn">選擇店鋪</button>
       </div>
     </>
