0d2d673c93f9ca3809c6c51d6740b3ed918a7840
chengen0612 <chengen0612@gmail.com>
2021-07-08 23:54:24 +0800

    dev: import all block into index.js of Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
+
+import HomeHeader from './HomeHeader'
+import HomeCustomized from './HomeCustomized'
+import HomeCourse from './HomeCourse'
+import HomeBestseller from './HomeBestseller/HomeBestseller'
 import HomeMap from './HomeMap'
+import HomeFooter from './HomeFooter'
+import HomeAside from './HomeAside'
 
 function Home() {
   return (
     <>
+      <HomeHeader />
+      <HomeCustomized />
+      <HomeCourse />
+      <HomeBestseller />
       <HomeMap />
+      <HomeFooter />
+      <HomeAside />
     </>
   )
 }
