2a4c9572273b7114fb707beb0afaaafb3549c913
chengen0612 <chengen0612@gmail.com>
2021-07-10 19:39:45 +0800

    dev: import every pages into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css'
 import Home from './pages/Home'
+import Bestseller from './pages/Bestseller'
+import Course from './pages/Course'
+import Custom from './pages/Custom'
+import Checkout from './pages/Checkout'
+import Member from './pages/Member'
+import Official from './pages/Official'
 import * as Icon from 'react-feather' // icon
 
 function App() {
   return (
     <>
       <Home />
+      <Bestseller />
+      <Course />
+      <Custom />
+      <Checkout />
+      <Member />
+      <Official />
     </>
   )
 }
