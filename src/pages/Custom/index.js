af223b548f2b1ac928a184f745066e7b07ac06ad
chengen0612 <79037530+chengen0612@users.noreply.github.com>
2021-07-11 16:53:37 +0800

    update: add a little bit

--- src/pages/Custom/index.js
+++ src/pages/Custom/index.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import { FiX } from 'react-icons/fi'
+import { imgPath } from '../../config'
+import './style.scss'
 
 function Custom() {
-  return <></>
+  return (
+    <>
+      <div className="custom">
+        <div className="custom__progress-bar">
+          <div className="custom__progress-note">前調</div>
+          <div className="custom__progress-note">中調</div>
+          <div className="custom__progress-note">後調</div>
+        </div>
+        <FiX className="custom__close-btn" />
+        <p className="custom__description"></p>
+        <div className="custom__fragrance-bar">
+          {/* <img className="custom__fragrance-fruit" alt="" />
+          <img className="custom__fragrance-flower" alt="" />
+          <img className="custom__fragrance-leaf" alt="" />
+          <img className="custom__fragrance-herb" alt="" /> */}
+          <img
+            className="custom__fragrance-symbol"
+            src={imgPath + '/images/custom/sidebar_flower.png'}
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Custom
