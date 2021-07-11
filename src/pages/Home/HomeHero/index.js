7733429361125635b6699a59d47683fa0d61001c
chengen0612 <79037530+chengen0612@users.noreply.github.com>
2021-07-11 10:46:20 +0800

    refactor: seperate HomeHeader into common component Header and HomeHero under Home directory

---
+++ src/pages/Home/HomeHero/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+function HomeHero() {
+  return (
+    <>
+      <div className="header__page container-fluid p-0"></div>
+    </>
+  )
+}
+export default HomeHero
