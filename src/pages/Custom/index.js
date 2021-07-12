6a28d93863b14099b3cc5a0d0a85e0eb00f99a25
chengen0612 <chengen0612@gmail.com>
2021-07-12 08:29:25 +0800

    update: add buttons

--- src/pages/Custom/index.js
+++ src/pages/Custom/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { FiX } from 'react-icons/fi'
+import {
+  FiX,
+  FiRefreshCw,
+  FiSkipBack,
+  FiCheckSquare,
+  FiHeart,
+  FiShoppingBag,
+  FiUpload,
+} from 'react-icons/fi'
 import { imgPath } from '../../config'
 import './style.scss'
 
@@ -14,17 +22,41 @@ function Custom() {
         </div>
         <FiX className="custom__close-btn" />
         <p className="custom__description"></p>
+        <div className="custom__bottle"></div>
         <div className="custom__fragrance-bar">
-          {/* <img className="custom__fragrance-fruit" alt="" />
-          <img className="custom__fragrance-flower" alt="" />
-          <img className="custom__fragrance-leaf" alt="" />
-          <img className="custom__fragrance-herb" alt="" /> */}
           <img
-            className="custom__fragrance-symbol"
+            className="custom__fragrance-fruit"
+            src={imgPath + '/images/custom/sidebar_fruit.png'}
+            alt=""
+          />
+          <img
+            className="custom__fragrance-flower"
             src={imgPath + '/images/custom/sidebar_flower.png'}
             alt=""
           />
+          <img
+            className="custom__fragrance-leaf"
+            src={imgPath + '/images/custom/sidebar_leaf.png'}
+            alt=""
+          />
+          <img
+            className="custom__fragrance-herb"
+            src={imgPath + '/images/custom/sidebar_herb.png'}
+            alt=""
+          />
         </div>
+        <button className="btn custom__btn-reset">
+          <FiRefreshCw className="feather-m custom__icon"/>
+          重做
+        </button>
+        <button className="btn custom__btn-goback">
+          <FiSkipBack />
+          退回
+        </button>
+        <button className="btn custom__btn-complete">
+          <FiCheckSquare />
+          完成
+        </button>
       </div>
     </>
   )
