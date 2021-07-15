cce847fad1d75bf56f5ded9804a0218b9294bee9
chengen0612 <79037530+chengen0612@users.noreply.github.com>
2021-07-15 17:37:35 +0800

    dev: prepare files for route switching

---
+++ .tmp/switcher/App.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+// 共通元件
+import Header from './components/Header'
+import SecondaryFooter from './components/SecondaryFooter'
+
+// 獨立分頁
+import Home from './pages/Home'
+import Bestseller from './pages/Bestseller'
+import Custom from './pages/Custom'
+import Checkout from './pages/Checkout'
+import LoginAndRegister from './pages/LoginAndRegister'
+
+// 分頁集合
+import SwitchMember from './routes/SwitchMember'
+import SwitchOfficial from './routes/SwitchOfficial'
+import SwitchCourse from './routes/SwitchCourse'
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Switch>
+          <Route path="/bestseller">
+            <Header />
+            <Bestseller />
+            <SecondaryFooter />
+          </Route>
+          <Route path="/custom">
+            <Custom />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+            <SecondaryFooter />
+          </Route>
+
+          {/* switch member */}
+          <Route path="/member">
+            <SwitchMember />
+          </Route>
+
+          {/* switch official */}
+          <Route path="/official">
+            <SwitchOfficial />
+          </Route>
+
+          {/* switch course */}
+          <Route path="/course">
+            <SwitchCourse />
+          </Route>
+
+          <Route path="/login">
+            <Header />
+            <LoginAndRegister />
+            <SecondaryFooter />
+          </Route>
+          <Route exact path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </>
+    </Router>
+  )
+}
+
+export default App
---
+++ .tmp/switcher/routes/SwitchMember.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { BrowserRouter as Route, Switch } from 'react-router-dom'
+
+// 共通元件
+import Header from '../components/Header'
+
+// 分頁元件
+import Member from '../pages/Member'
+import MemberData from '../pages/Member/MemberData'
+import MemberFavorites from '../pages/Member/MemberFavorites'
+import MemberOrder from '../pages/Member/MemberOrder'
+
+function SwitchMember() {
+  return (
+    <>
+      <Switch>
+        <Route path="/member/data">
+          <Header />
+          <MemberData />
+        </Route>
+        <Route path="/member/favorites">
+          <Header />
+          <MemberFavorites />
+        </Route>
+        <Route path="/member/order">
+          <Header />
+          <MemberOrder />
+        </Route>
+        <Route path="/member">
+          <Header />
+          <Member />
+        </Route>
+      </Switch>
+    </>
+  )
+}
+
+export default SwitchMember
