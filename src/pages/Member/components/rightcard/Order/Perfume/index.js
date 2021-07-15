65523490fa49fd7a836f124a2862d3c995186919
norman <a07401234@gmail.com>
2021-07-15 22:13:58 +0800

    update: add Order blocks Course and Perfume

---
+++ src/pages/Member/components/rightcard/Order/Perfume/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+import { FiEdit } from 'react-icons/fi'
+
+function MemberLeftCard() {
+  return (
+    <>
+      <div className="member__rightCard"></div>
+    </>
+  )
+}
+
+export default MemberLeftCard
