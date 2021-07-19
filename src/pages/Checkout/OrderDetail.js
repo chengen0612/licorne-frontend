2e64a129e709c67089cd88418836e1848a3104d0
wandererwen <80301765+wandererwen@users.noreply.github.com>
2021-07-20 00:33:58 +0800

    update: fetch data (member) to render member info in order details and address edit modal

--- src/pages/Checkout/OrderDetail.js
+++ src/pages/Checkout/OrderDetail.js
@@ -1,9 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FiEdit } from 'react-icons/fi'
 import AddressEditModal from './components/AddressEditModal'
 import Backdrop from './components/Backdrop'
 
 function OrderDetail() {
+  const [members, setMembers] = useState([])
+  async function getMemberInfoFromServer() {
+    // 連接的伺服器資料網址
+    const url = 'http://localhost:6005/checkout'
+
+    // 注意header資料格式要設定，伺服器才知道是json格式
+    const request = new Request(url, {
+      method: 'GET',
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }),
+    })
+
+    const response = await fetch(request)
+    const data = await response.json()
+    const memberInfo = data.member
+    console.log(memberInfo)
+    // 設定資料
+    setMembers(memberInfo)
+  }
+
+  useEffect(() => {
+    getMemberInfoFromServer()
+  }, [])
+
   const [showModal, setShowModal] = useState()
   function showModalHandler() {
     setShowModal(true)
@@ -21,7 +47,7 @@ function OrderDetail() {
           <span className="checkout__order-box-delivery-title">運送方式</span>
           <div className="checkout__order-box-delivery-labels">
             <label htmlFor="" className="checkout__order-box-delivery-address">
-              <input className="radio" type="radio" name="delivery" />
+              <input className="radio" type="radio" name="delivery" checked />
               指定地址
             </label>
             <label htmlFor="" className="checkout__order-box-delivery-store">
@@ -30,21 +56,45 @@ function OrderDetail() {
             </label>
           </div>
           <div className="checkout__order-box-delivery-edit-bg d-flex flex-column p-3 mt-2 mb-2">
-            <div className="checkout__order-box-delivery-edit-wrapper d-flex justify-content-between">
-              <span className="checkout__order-box-recipient">哭肉狗狗</span>
-              <FiEdit
-                className="feather-s"
-                role="button"
-                onClick={showModalHandler}
-              />
-            </div>
-            <span className="checkout__order-box-recipient-phone">
-              (+886) 912 345 678
-            </span>
-            <span className="checkout__order-box-recipient-address">
-              29850桃園市桃園區中正路100巷100號
-            </span>
-            <span className="checkout__order-box-buyer">訂購人：同收件人</span>
+            {members.map((v, i) => {
+              const member = v[0]
+              return (
+                <>
+                  <div className="checkout__order-box-delivery-edit-wrapper d-flex justify-content-between">
+                    <span
+                      className="checkout__order-box-recipient"
+                      key={member.id}
+                    >
+                      {/* 哭肉狗狗 */}
+                      {member.member_name}
+                    </span>
+                    <FiEdit
+                      className="feather-s"
+                      role="button"
+                      onClick={showModalHandler}
+                    />
+                  </div>
+
+                  <span
+                    className="checkout__order-box-recipient-phone"
+                    key={member.id}
+                  >
+                    {/* (+886) 912 345 678 */}
+                    {member.member_phone}
+                  </span>
+                  <span
+                    className="checkout__order-box-recipient-address"
+                    key={member.id}
+                  >
+                    {/* 29850桃園市桃園區中正路100巷100號 */}
+                    {member.member_address}
+                  </span>
+                  <span className="checkout__order-box-buyer">
+                    訂購人：同收件人
+                  </span>
+                </>
+              )
+            })}
           </div>
           {showModal && <Backdrop onClick={closeModalHandler} />}
           {showModal && <AddressEditModal onClose={closeModalHandler} />}
@@ -53,10 +103,10 @@ function OrderDetail() {
           <span className="checkout__order-box-payment-title">付款方式</span>
           <div className="checkout__order-box-payment-labels">
             <label className="checkout__order-box-payment-card">
-              <input className="radio" type="radio" name="payment" />
+              <input className="radio" type="radio" name="payment" checked />
               信用卡
             </label>
-            <label className="checkout__order-box-payment-cash pl-4">
+            <label className="checkout__order-box-payment-cash">
               <input className="radio" type="radio" name="payment" />
               貨到付款
             </label>
--- src/pages/Checkout/components/AddressEditModal.js
+++ src/pages/Checkout/components/AddressEditModal.js
@@ -1,16 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FiX } from 'react-icons/fi'
 
 function AddressEditModal(props) {
-  const [username, setUsername] = useState('哭肉狗狗')
-  const [phone, setPhone] = useState('+886 912 345 678')
-  const [address, setAddress] = useState('29850 桃園市桃園區中正路100巷100號')
-  const [recipient, setRecipient] = useState('哭肉狗狗')
-  const [recipientPhone, setRecipientPhone] = useState('+886 912 345 678')
-  const [recipientAddress, setRecipientAddress] = useState(
-    '29850 桃園市桃園區中正路100巷100號'
-  )
+  const [members, setMembers] = useState([])
+  // const [username, setUsername] = useState('哭肉狗狗')
+  // const [phone, setPhone] = useState('+886 912 345 678')
+  // const [address, setAddress] = useState('29850 桃園市桃園區中正路100巷100號')
+  // const [recipient, setRecipient] = useState('哭肉狗狗')
+  // const [recipientPhone, setRecipientPhone] = useState('+886 912 345 678')
+  // const [recipientAddress, setRecipientAddress] = useState('29850 桃園市桃園區中正路100巷100號')
   const [same, setSame] = useState(true)
+
+  async function getMemberInfoFromServer() {
+    // 連接的伺服器資料網址
+    const url = 'http://localhost:6005/checkout'
+
+    // 注意header資料格式要設定，伺服器才知道是json格式
+    const request = new Request(url, {
+      method: 'GET',
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }),
+    })
+
+    const response = await fetch(request)
+    const data = await response.json()
+    const memberInfo = data.member
+    console.log(memberInfo)
+    // 設定資料
+    setMembers(memberInfo)
+  }
+
+  useEffect(() => {
+    getMemberInfoFromServer()
+  }, [])
 
   function editHandler() {
     props.onEdit()
@@ -30,98 +54,111 @@ function AddressEditModal(props) {
       <span className="checkout__address-edit-modal-title align-self-center">
         編輯收件資訊
       </span>
-      <form className="d-flex flex-column p-4">
-        <span className="checkout__address-edit-modal-title pb-2">訂購人</span>
-        <label className="checkout__address-edit-modal-label" htmlFor="">
-          姓名
-          <input
-            name="username"
-            type="text"
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value)
-            }}
-            required
-          />
-        </label>
-        <label className="checkout__address-edit-modal-label" htmlFor="">
-          電話
-          <input
-            name="phone"
-            type="text"
-            value={phone}
-            onChange={(e) => {
-              setPhone(e.target.value)
-            }}
-            required
-          />
-        </label>
-        <label className="checkout__address-edit-modal-label" htmlFor="">
-          地址
-          <input
-            name="address"
-            type="text"
-            value={address}
-            onChange={(e) => {
-              setAddress(e.target.value)
-            }}
-            required
-          />
-        </label>
-        <span className="checkout__address-edit-modal-title">收件人</span>
-        <label className="checkout__address-edit-modal-label" htmlFor="">
-          姓名
-          <input
-            name="recipient"
-            type="text"
-            value={recipient}
-            onChange={(e) => {
-              setRecipient(e.target.value)
-            }}
-            required
-          />
-        </label>
-        <label className="checkout__address-edit-modal-label" htmlFor="">
-          電話
-          <input
-            name="recipientPhone"
-            type="text"
-            value={recipientPhone}
-            onChange={(e) => {
-              setRecipientPhone(e.target.value)
-            }}
-            required
-          />
-        </label>
-        <label className="checkout__address-edit-modal-label" htmlFor="">
-          地址
-          <input
-            name="recipientAddress"
-            type="text"
-            value={recipientAddress}
-            onChange={(e) => {
-              setRecipientAddress(e.target.value)
-            }}
-            required
-          />
-        </label>
-        <label className="checkbox-label" htmlFor="">
-          <input
-            type="checkbox"
-            checked={same}
-            onChange={(e) => {
-              setSame(e.target.checked)
-            }}
-          />
-          收件資訊同上
-        </label>
-        <button
-          className="checkout__address-edit-modal-btn align-self-end"
-          onClick={editHandler}
-        >
-          更新地址
-        </button>
-      </form>
+      {members.map((v, i) => {
+        const member = v[0]
+        return (
+          <form className="d-flex flex-column p-4">
+            <span className="checkout__address-edit-modal-title pb-2">
+              訂購人
+            </span>
+            <label className="checkout__address-edit-modal-label" htmlFor="">
+              姓名
+              <input
+                name="username"
+                type="text"
+                key={member.id}
+                defaultValue={member.member_name}
+                // onChange={(e) => {
+                //   setUsername(e.target.value)
+                // }}
+                required
+              />
+            </label>
+            <label className="checkout__address-edit-modal-label" htmlFor="">
+              電話
+              <input
+                name="phone"
+                type="text"
+                key={member.id}
+                defaultValue={member.member_phone}
+                // onChange={(e) => {
+                //   setPhone(e.target.value)
+                // }}
+                required
+              />
+            </label>
+            <label className="checkout__address-edit-modal-label" htmlFor="">
+              地址
+              <input
+                name="address"
+                type="text"
+                key={member.id}
+                defaultValue={member.member_address}
+                // onChange={(e) => {
+                //   setAddress(e.target.value)
+                // }}
+                required
+              />
+            </label>
+            <span className="checkout__address-edit-modal-title">收件人</span>
+            <label className="checkout__address-edit-modal-label" htmlFor="">
+              姓名
+              <input
+                name="recipient"
+                type="text"
+                key={member.id}
+                defaultValue={member.member_name}
+                // onChange={(e) => {
+                //   setRecipient(e.target.value)
+                // }}
+                required
+              />
+            </label>
+            <label className="checkout__address-edit-modal-label" htmlFor="">
+              電話
+              <input
+                name="recipientPhone"
+                type="text"
+                key={member.id}
+                defaultValue={member.member_phone}
+                // onChange={(e) => {
+                //   setRecipientPhone(e.target.value)
+                // }}
+                required
+              />
+            </label>
+            <label className="checkout__address-edit-modal-label" htmlFor="">
+              地址
+              <input
+                name="recipientAddress"
+                type="text"
+                key={member.id}
+                value={member.member_receive}
+                // onChange={(e) => {
+                //   setRecipientAddress(e.target.value)
+                // }}
+                required
+              />
+            </label>
+            <label className="checkbox-label" htmlFor="">
+              <input
+                type="checkbox"
+                checked={same}
+                // onChange={(e) => {
+                //   setSame(e.target.checked)
+                // }}
+              />
+              收件資訊同上
+            </label>
+            <button
+              className="checkout__address-edit-modal-btn align-self-end"
+              onClick={editHandler}
+            >
+              更新地址
+            </button>
+          </form>
+        )
+      })}
     </div>
   )
 }
