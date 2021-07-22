b8520bb128ebfbecbaca6dbc8529594068fe719a
chengen0612 <79037530+chengen0612@users.noreply.github.com>
2021-07-22 17:55:18 +0800

    update: add data for custom notes introduction

---
+++ src/pages/Custom/Introduction/Notes/data.js
@@ -0,0 +1,28 @@
+const topNote = {
+  title: '前調',
+  titleEn: 'Top Notes',
+  explanation:
+    '前調又可稱作前味、高音調，通常是由比較小的分子所構成，大約在10分鐘左右所散發出來的香氣。味道比較容易讓人有煥然一新、提起精神的感受，也像一首旋律中的高音引人注目、與令人印象深刻。\n\n常用的前調，比較會是屬於果香、花香調。這類香氣往往能給人一種清新感，而且酸酸甜甜的，容易讓人著迷。\n\n其常用的有像是薄荷、檸檬、柑橘、佛手柑、快樂鼠尾草、薰衣草等等。\n',
+  types: [
+    {
+      name: '柑橘香調',
+      nameEn: 'Citrus',
+      list: ['檸檬', '甜橙', '柚子'],
+      image: '/images/custom/intro/notes/top-citrus.png',
+    },
+    {
+      name: '果香調',
+      nameEn: 'Fruity',
+      list: ['蜜桃', '蘋果', '紅莓'],
+      image: '/images/custom/intro/notes/top-fruity.png',
+    },
+    {
+      name: '草本香調',
+      nameEn: 'Herbal',
+      list: ['薰衣草', '鼠尾草', '薄荷'],
+      image: '/images/custom/intro/notes/top-herbal.png',
+    },
+  ],
+}
+
+export { topNote }
