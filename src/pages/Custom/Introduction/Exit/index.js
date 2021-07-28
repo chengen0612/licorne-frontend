2bfa1cb61cf86bb44db31c856eddc6360b84571b
chengen0612 <79037530+chengen0612@users.noreply.github.com>
2021-07-28 17:47:08 +0800

    update: develop exit page of custom introduction

--- src/pages/Custom/Introduction/Exit/index.js
+++ src/pages/Custom/Introduction/Exit/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './style.scss'
 
 function Exit() {
-  return <></>
+  return (
+    <>
+      <div className="c-intro-exit">
+        <div className="c-intro-exit__r-blob"></div>
+        <h3 className="c-intro-exit__greet">學習完香水的相關知識</h3>
+        <h3 className="c-intro-exit__greet">讓我們來製作你的專屬香水吧！</h3>
+        <div className="c-intro-exit__ahead-btn">
+          <Link to="/custom">開始製作</Link>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Exit
