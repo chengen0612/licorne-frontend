f5692c4fe8f71ea57f1cd32ebca0073005bfe95c
chengen0612 <79037530+chengen0612@users.noreply.github.com>
2021-07-28 14:44:48 +0800

    update: add custom introduction entrance page

---
+++ src/pages/Custom/Entrance/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { FiX } from 'react-icons/fi'
+
+import './style.scss'
+
+function Entrance() {
+  /* eslint-disable */
+  return (
+    <>
+      <div className="c-entrance">
+        <h3 className="c-entrance__greet">是否有過製作香水的經驗呢</h3>
+        <div className="c-entrance__btn-wrap">
+          <Link to="/custom" className="c-entrance__btn">是</Link>
+          <Link to="/custom/introduction" className="c-entrance__btn">否</Link>
+        </div>
+        <Link to="/" >
+          <FiX className="c-entrance__close-btn" />
+        </Link>
+      </div>
+    </>
+  )
+  /* eslint-enable */
+}
+
+export default Entrance
