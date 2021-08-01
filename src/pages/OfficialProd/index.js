a80a564778b5fb4fe7c11d423a872b33754f8df7
wandererwen <80301765+wandererwen@users.noreply.github.com>
2021-08-01 21:40:49 +0800

    update: request data from db

--- src/pages/OfficialProd/index.js
+++ src/pages/OfficialProd/index.js
@@ -7,20 +7,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Recommendation from './Recommendation'
 import MainProduct from './MainProduct'
 
+const getDataFromServer = async () => {
+  const params = { params: { officialId: 1 } }
+  const url = 'http://localhost:6005/officialid'
+  const response = await axios.get(url, params)
+  console.log(response)
+}
+
 function OfficialProd(props) {
   const [item, setItem] = useState([])
-  const getDataFromServer = async () => {
-    const params = props.match.params
-    const url = 'http://localhost:6005/officialid'
-    const response = await axios.get(url, params)
-    console.log(response)
-    // this would be the path in the browser
-    // http://localhost:3000/official/1
-    // const productId = params.id
-    const product = response.data.product
-    console.log('product info', product)
-    setItem(product)
-  }
 
   useEffect(() => {
     getDataFromServer()
