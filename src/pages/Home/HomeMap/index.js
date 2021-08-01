6940d427c20738f23fb27373e1810e66ad87a2c6
chengen0612 <79037530+chengen0612@users.noreply.github.com>
2021-08-01 16:47:18 +0800

    update: transfer codes from teammate's system, build up basic settings (thanks for loveblue0121's help)

--- src/pages/Home/HomeMap/index.js
+++ src/pages/Home/HomeMap/index.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.css'
 import MyMap from './MyMap'
 import MapCard from './MapCard'
 
 function HomeMap() {
+  const [placeLatLng, setPlaceLatLng] = useState([])
+
+  useEffect(() => {
+    getCourseFromServer()
+  }, [])
+
+  async function getCourseFromServer() {
+    const url = `http://localhost:6005/course`
+    const request = new Request(url, {
+      method: 'GET',
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'appliaction/json',
+      }),
+    })
+    const response = await fetch(request)
+    const data = await response.json()
+
+    setPlaceLatLng(data.place)
+  }
   return (
     <>
       <div className="map">
-        <MyMap />
+        <MyMap placeLatLng={placeLatLng} />
         <MapCard />
       </div>
     </>
