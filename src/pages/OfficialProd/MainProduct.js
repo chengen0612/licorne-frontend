0ead67b3dafbc0f825982daf87311f89bc802e97
wandererwen <80301765+wandererwen@users.noreply.github.com>
2021-08-04 06:01:02 +0800

    feat: change corresponding path when selecting different volume size

--- src/pages/OfficialProd/MainProduct.js
+++ src/pages/OfficialProd/MainProduct.js
@@ -3,27 +3,23 @@ import './style.scss'
 import 'bootstrap/dist/css/bootstrap.css'
 // import { imgPath } from '../../config'
 import { FiHeart, FiChevronDown } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 import { GiWaterDrop } from 'react-icons/gi'
 import ProductAccordion from './components/ProductAccordion'
 
-function MainProduct({ item }) {
-  const options = [
-    {
-      volume: '50ML',
-      price: 3500,
-    },
-    {
-      volume: '100ML',
-      price: 5000,
-    },
-  ]
-
-  console.log('product info', item)
-
+function MainProduct({ id, item, siblingItem }) {
   const [active, setActive] = useState(true)
   const toggleClass = () => {
     setActive(!active)
   }
+
+  // 判別是否為偶數
+  function isEven(value) {
+    if (value % 2 === 0) return value - 1
+    else return value + 1
+  }
+  const siblingId = isEven(Number(id))
+  console.log('siblingId', siblingId)
 
   return (
     <>
@@ -45,6 +41,8 @@ function MainProduct({ item }) {
             {/* 大自然香氛 */}
             {item.series_name}
           </span>
+          {/* TODO: display selected option */}
+          {/* FIXME: option in descending order */}
           <div
             className={active ? 'official__dropdown-menu-active' : null}
             id="official__dropdown-menu"
@@ -54,18 +52,37 @@ function MainProduct({ item }) {
               請選擇容量 <FiChevronDown />
             </div>
             <div className="official__dropdown-content">
-              {options.map((option, i) => {
+              <div className="official__select-options">
+                瓶裝 {item.volume}
+                <span className="d-flex">
+                  NT$ {item.price}
+                  &nbsp;&nbsp;&nbsp;
+                  <GiWaterDrop />
+                </span>
+              </div>
+              {/* FIXME: weird transition */}
+              <Link to={`/official/${siblingId}`}>
+                <div className="official__select-options">
+                  瓶裝 {siblingItem.volume}
+                  <span className="d-flex">
+                    NT$ {siblingItem.price}
+                    &nbsp;&nbsp;&nbsp;
+                    <GiWaterDrop />
+                  </span>
+                </div>
+              </Link>
+              {/* {options.map((option, i) => {
                 return (
                   <div key={i} className="official__select-options">
                     瓶裝 {option.volume}
                     <span className="d-flex">
-                      NT$ {option.price}
+                      NT$ {item.price}
                       &nbsp;&nbsp;&nbsp;
                       <GiWaterDrop />
                     </span>
                   </div>
                 )
-              })}
+              })} */}
             </div>
           </div>
           <div className="official__btn-group">
