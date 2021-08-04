b6f74f0c1e59e72a057a934a863ebda6e7c16fee
wandererwen <80301765+wandererwen@users.noreply.github.com>
2021-08-05 00:04:01 +0800

    update: change classnames according to bem naming rules and rearrange guide section

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -1,73 +1,91 @@
 import React from 'react'
-import './style.css'
+import './style.scss'
 import { imgPath } from '../../config'
+import { Link } from 'react-router-dom'
 import Sidebar from './Sidebar'
 
 function Quiz() {
   return (
     <>
-      <section>
-        <div className="info">
-          <Sidebar />
-          <div className="title">
-            <h1>尋找你的專屬香氣</h1>
-          </div>
-          <div className="textBox">
-            <p>
-              外在形象是每個人一輩子都必須面對的課題，
-              人類對不同氣味會作出直接的反應， 很多人會在不知不覺中
-              利用嗅覺認知建立對人或事直觀且深刻的印象，
-              你是否了解你給人最主觀的印象呢?
-              藉由這個簡單的測驗，來尋找專屬於你的香味吧!
+      <Sidebar />
+      <div className="pageWrapper">
+        <section className="quiz">
+          <div className="quiz__info">
+            <div className="quiz__info-title">
+              <h3>尋找你的專屬香氣</h3>
+            </div>
+            <p className="quiz__info-intro">
+              外在形象是每個人一輩子都必須面對的課題 <br />
+              人類對不同氣味會作出直接的反應 <br />
+              很多人會在不知不覺中 <br />
+              利用嗅覺認知建立對人或事直觀且深刻的印象 <br />
+              你是否了解你給人最主觀的印象呢？ <br />
+              藉由這個簡單的測驗，來尋找專屬於你的香味吧！
             </p>
+            <div className="quiz__start">
+              <button className="quiz__start-btn">開始</button>
+            </div>
           </div>
-          <div className="btn">
-            <button className="start">開始</button>
+        </section>
+      </div>
+      <div className="quiz__img-box">
+        <img
+          className="quiz__img"
+          src={imgPath + '/images/quiz/home-quiz.jpg'}
+          alt=""
+        />
+      </div>
+      <article className="quiz__guide">
+        <h5 className="quiz__guide-text">
+          香水是令人愉悅的 <br />
+          小小的一瓶香水能讓人擺脫糟糕的心情
+          <br />
+          幾滴自己喜愛的香水味能讓你心頭的陰霾一掃而空 <br />
+          只留下一片明媚與晴朗
+          <br />
+        </h5>
+        {/* 客製化 */}
+        <div className="quiz__guide-items">
+          <div className="quiz__guide-item">
+            <Link to="/custom/entrance" className="quiz__guide-item-link">
+              <div className="quiz__guide-item-box">
+                <img
+                  className="quiz__guide-img-custom"
+                  src={imgPath + '/images/quiz/home-custom.jpg'}
+                  alt="custom"
+                />
+              </div>
+            </Link>
+            <h4 className="quiz__guide-item-text">香水客製化</h4>
           </div>
-        </div>
-      </section>
-      <section className="imgSection">
-        <div className="imgBox">
-          <img
-            className="img"
-            src={imgPath + '/images/quiz/home-quiz.jpg'}
-            alt=""
-          />
-        </div>
-      </section>
-      <article>
-        <div className="info_down">
-          <p>前往其他頁面文字引導</p>
-        </div>
-        <div className="link">
-          <div className="box">
-            <img
-              className="custom"
-              src={imgPath + '/images/quiz/home-custom.jpg'}
-              alt=""
-            />
+          <div className="quiz__guide-divider"></div>
+          {/* 課程 */}
+          <div className="quiz__guide-item">
+            <Link to="/course" className="quiz__guide-item-link">
+              <div className="quiz__guide-item-box">
+                <img
+                  className="quiz__guide-img-course"
+                  src={imgPath + '/images/quiz/home-course.jpg'}
+                  alt="course"
+                />
+              </div>
+            </Link>
+            <h4 className="quiz__guide-item-text">工作坊課程</h4>
           </div>
-          <div className="line_box"></div>
-          <div className="box">
-            <img
-              className="course"
-              src={imgPath + '/images/quiz/home-course.jpg'}
-              alt=""
-            />
+          <div className="quiz__guide-divider"></div>
+          {/* 排行榜 */}
+          <div className="quiz__guide-item">
+            <Link to="/bestseller" className="quiz__guide-item-link">
+              <div className="quiz__guide-item-box">
+                <img
+                  className="quiz__guide-img-bestseller"
+                  src={imgPath + '/images/quiz/home-bestseller.jpg'}
+                  alt="course"
+                />
+              </div>
+            </Link>
+            <h4 className="quiz__guide-item-text">熱銷排行榜</h4>
           </div>
-          <div className="line_box"></div>
-          <div className="box">
-            <img
-              className="bestseller"
-              src={imgPath + '/images/quiz/home-bestseller.jpg'}
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="text">
-          <h3>香水客製化</h3>
-          <h3>工作坊課程</h3>
-          <h3>熱銷排行榜</h3>
         </div>
       </article>
     </>
