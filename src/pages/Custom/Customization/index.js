a0ab0c0e8d1803bef2c85a40ecdf5bba94b2bbb1
chengen0612 <chengen0612@gmail.com>
2021-08-05 23:23:41 +0800

    update: add spinner to customization

--- src/pages/Custom/Customization/index.js
+++ src/pages/Custom/Customization/index.js
@@ -1,17 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './style.scss'
 
+// components
+import Spinner from '../../../components/Spinner'
 import Process from './Process'
 import Result from './Result'
 
 function Customization() {
+  const [didMount, setDidMount] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const [productDetail, setProductDetail] = useState({})
-  // console.log(Object.keys(productDetail))
+
+  useEffect(() => {
+    setDidMount(true)
+  }, [])
+
+  // handle spinner
+  useEffect(() => {
+    if (didMount === false) return
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 500)
+  }, [productDetail])
 
   return (
     <>
-      {Object.keys(productDetail).length === 0 ? (
+      {isLoading ? (
+        <Spinner />
+      ) : Object.keys(productDetail).length === 0 ? (
         <Process setProductDetail={setProductDetail} />
       ) : (
         <Result
