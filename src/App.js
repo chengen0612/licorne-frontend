25d36703c6c8fcfefab8a67f9c23513a99c8d8ad
chengen0612 <chengen0612@gmail.com>
2021-08-06 22:40:13 +0800

    BREAKING CHANGE(app): reorganize structure of router in order to add spinner animation to whole application where needed

--- src/App.js
+++ src/App.js
@@ -1,90 +1,15 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-// 共通元件
-import Header from './components/Header'
-import SecondaryFooter from './components/SecondaryFooter'
-
-// 獨立分頁
-import Home from './pages/Home'
-import Bestseller from './pages/Bestseller'
-import Checkout from './pages/Checkout'
-import LoginAndRegister from './pages/LoginAndRegister'
-import Payment from './pages/Payment'
-import Member from './pages/Member'
-import Official from './pages/Official'
-import OfficialProd from './pages/OfficialProd'
-import Quiz from './pages/Quiz'
-
-// 分頁子路由器
-// import SwitchMember from './routes/SwitchMember'
-// import SwitchOfficial from './routes/SwitchOfficial'
-import SwitchCourse from './routes/SwitchCourse'
-import SwitchCustom from './routes/SwitchCustom'
+import Direction from './Direction'
 
 function App() {
   return (
     <>
       <Router>
-        <Switch>
-          {/* middleware */}
-          <Route path="/course">
-            <SwitchCourse />
-          </Route>
-          <Route path="/custom">
-            <SwitchCustom />
-          </Route>
-
-          {/* direct path */}
-          <Route path="/official/:id">
-            <Header />
-            <OfficialProd />
-            <SecondaryFooter />
-          </Route>
-          <Route path="/official">
-            <Official />
-          </Route>
-          <Route path="/bestseller">
-            <Header />
-            <Bestseller />
-            <SecondaryFooter />
-          </Route>
-          <Route path="/quiz">
-            <Header />
-            <Quiz />
-          </Route>
-          <Route path="/checkout/payment">
-            <Payment />
-          </Route>
-          <Route path="/checkout">
-            <Header />
-            <Checkout />
-            <SecondaryFooter />
-          </Route>
-          <Route path="/member">
-            <Header />
-            <Member />
-          </Route>
-          <Route path="/login">
-            <Header />
-            <LoginAndRegister />
-            <SecondaryFooter />
-          </Route>
-          <Route exact path="/">
-            <Header />
-            <Home />
-          </Route>
-
-          {/* abandoned */}
-          {/* switch member */}
-          {/* <Route path="/member">
-            <SwitchMember />
-          </Route> */}
-          {/* switch official */}
-          {/* <Route path="/official">
-            <SwitchOfficial />
-          </Route> */}
-        </Switch>
+        <Route path="*">
+          <Direction />
+        </Route>
       </Router>
     </>
   )
---
+++ src/Direction.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from 'react'
+import { Route, Switch, useLocation } from 'react-router-dom'
+
+// 共通元件
+import Header from './components/Header'
+import SecondaryFooter from './components/SecondaryFooter'
+import Spinner from './components/Spinner'
+
+// 獨立分頁
+import Home from './pages/Home'
+import Bestseller from './pages/Bestseller'
+import Checkout from './pages/Checkout'
+import LoginAndRegister from './pages/LoginAndRegister'
+import Payment from './pages/Payment'
+import Member from './pages/Member'
+import Official from './pages/Official'
+import OfficialProd from './pages/OfficialProd'
+import Quiz from './pages/Quiz'
+
+// 分頁子路由器
+// import SwitchMember from './routes/SwitchMember'
+// import SwitchOfficial from './routes/SwitchOfficial'
+import SwitchCourse from './routes/SwitchCourse'
+import SwitchCustom from './routes/SwitchCustom'
+
+function Location() {
+  const location = useLocation()
+  const [useHeader, setUseHeader] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  // array to store pages not using header
+  /* eslint-disable */
+  const noSpinnerList = ['/custom/entrance', '/custom/introduction', '/custom/process', '/member', '/member/profile', '/member/order', '/member/favorites', '/official',]
+  /* eslint-enable */
+
+  const handleHeader = () => {
+    const params = location.pathname.split('/')[1]
+    // params !== 'custom' ? setUseHeader(true) : setUseHeader(false)
+    switch (params) {
+      case 'custom':
+      case 'official':
+        setUseHeader(false)
+        break
+      default:
+        setUseHeader(true)
+        break
+    }
+  }
+
+  const handleSpinner = () => {
+    const pathname = location.pathname
+    const index = noSpinnerList.findIndex((value) => value === pathname)
+    if (index > -1) return
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 500)
+  }
+
+  useEffect(() => {
+    handleHeader()
+    handleSpinner()
+  }, [location])
+
+  const switchRoutes = () => {
+    return (
+      <>
+        <Switch>
+          {/* middleware */}
+          <Route path="/course">
+            <SwitchCourse />
+          </Route>
+          <Route path="/custom">
+            <SwitchCustom />
+          </Route>
+
+          {/* direct path */}
+          <Route path="/official/:id">
+            <OfficialProd />
+            <SecondaryFooter />
+          </Route>
+          <Route path="/official">
+            <Official />
+          </Route>
+          <Route path="/bestseller">
+            <Bestseller />
+            <SecondaryFooter />
+          </Route>
+          <Route path="/quiz">
+            <Quiz />
+          </Route>
+          <Route path="/checkout/payment">
+            <Payment />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+            <SecondaryFooter />
+          </Route>
+          <Route path="/member">
+            <Member />
+          </Route>
+          <Route path="/login">
+            <LoginAndRegister />
+            <SecondaryFooter />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          {/* abandoned */}
+          {/* switch member */}
+          {/* <Route path="/member">
+            <SwitchMember />
+          </Route> */}
+          {/* switch official */}
+          {/* <Route path="/official">
+            <SwitchOfficial />
+          </Route> */}
+        </Switch>
+      </>
+    )
+  }
+
+  return (
+    <>
+      {useHeader && <Header />}
+      {isLoading ? <Spinner /> : switchRoutes()}
+    </>
+  )
+}
+
+export default Location
