8d032e7324b8aa32e683f573124ba77560cac766
chengen0612 <79037530+chengen0612@users.noreply.github.com>
2021-08-07 12:51:19 +0800

    update(bestseller): make spinner of bestseller it's own componenet, add related settings

---
+++ src/pages/Bestseller/Spinner/index.js
@@ -0,0 +1,11 @@
+import './style.scss'
+
+export default function Spinner() {
+  return (
+    <>
+      <div className="best__spinner-wrapper">
+        <div className="spinner-border" role="status"></div>
+      </div>
+    </>
+  )
+}
--- src/pages/Bestseller/index.js
+++ src/pages/Bestseller/index.js
@@ -3,16 +3,25 @@ import axios from 'axios'
 
 import './style.scss'
 
+// public components
+import Spinner from './Spinner'
+
 // components
 import ProductCard from './ProductCard'
 import Filters from './Filters'
 
 function Bestseller() {
+  // state for spinner
+  const [isAmount, setIsAmount] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  // state for filters
   const [checkedSeries, setCheckedSeries] = useState([])
   const [sortBy, setSortBy] = useState('每月人氣銷售')
+
+  // state for display
   const [display, setDisplay] = useState([])
-  // products not in top three
-  const [otherProducts, setOtherProducts] = useState([])
+  const [otherProducts, setOtherProducts] = useState([]) // products not in top three
 
   const getDataFromServer = async () => {
     const params = { params: { sortBy: sortBy, checkedSeries: checkedSeries } }
@@ -25,36 +34,57 @@ function Bestseller() {
     setOtherProducts(others)
   }
 
-  /* eslint-disable */
+  // first render
   useEffect(() => {
     getDataFromServer()
+    setIsAmount(true)
+  }, [])
+
+  // update by filter options
+  /* eslint-disable */
+  useEffect(() => {
+    if (isAmount === false) return
+    setIsLoading(true)
+    getDataFromServer()
+    setTimeout(() => { setIsLoading(false) }, 500)
   }, [checkedSeries, sortBy])
   /* eslint-enable */
+
+  const renderProducts = () => {
+    return (
+      <>
+        <div className="best__display-wrap">
+          <ProductCard key={0} data={display[0]} />
+        </div>
+        <div className="best__display-wrap">
+          <ProductCard key={1} data={display[1]} />
+          <ProductCard key={2} data={display[2]} />
+        </div>
+        <div className="best__display-wrap">
+          {otherProducts.map((item, i) => {
+            return <ProductCard key={i + 3} data={item} />
+          })}
+        </div>
+      </>
+    )
+  }
 
   return (
     <>
       {display.length > 0 && (
-        <div className="best-wrapper">
-          <h3 className="best__title">人氣熱銷排行榜</h3>
-          <Filters
-            checkedSeries={checkedSeries}
-            setCheckedSeries={setCheckedSeries}
-            sortBy={sortBy}
-            setSortBy={setSortBy}
-          />
-          <div className="best__display-wrap">
-            <ProductCard key={0} data={display[0]} />
+        <>
+          {isLoading && <Spinner />}
+          <div className="best-wrapper">
+            <h3 className="best__title">人氣熱銷排行榜</h3>
+            <Filters
+              checkedSeries={checkedSeries}
+              setCheckedSeries={setCheckedSeries}
+              sortBy={sortBy}
+              setSortBy={setSortBy}
+            />
+            {!isLoading && renderProducts()}
           </div>
-          <div className="best__display-wrap">
-            <ProductCard key={1} data={display[1]} />
-            <ProductCard key={2} data={display[2]} />
-          </div>
-          <div className="best__display-wrap">
-            {otherProducts.map((item, i) => {
-              return <ProductCard key={i + 3} data={item} />
-            })}
-          </div>
-        </div>
+        </>
       )}
     </>
   )
