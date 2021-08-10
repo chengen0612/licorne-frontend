7d295d150441c1cf454f7471c1f1a12c270fe397
wandererwen <80301765+wandererwen@users.noreply.github.com>
2021-08-10 13:41:23 +0800

    update: rename component name

--- src/pages/Custom/Customization/Result/CostList.js
+++ src/pages/Custom/Customization/Result/CostList.js
@@ -5,6 +5,11 @@ function CostList(props) {
 
   const total = +topNote.price + +middleNote.price + +baseNote.price
 
+  // add comma to price
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  }
+
   return (
     <>
       <table className="cust-res__cost-list">
@@ -12,31 +17,37 @@ function CostList(props) {
           <tr>
             <th></th>
             <th>材料</th>
-            <th>單價</th>
+            <th>&nbsp;&nbsp;&nbsp; 單價</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>前調</td>
             <td>{topNote.title}</td>
-            <td>NT$ {topNote.price}</td>
+            <td className="cust-res__price">
+              NT$ {numberWithCommas(topNote.price)}
+            </td>
           </tr>
           <tr>
             <td>中調</td>
             <td>{middleNote.title}</td>
-            <td>NT$ {middleNote.price}</td>
+            <td className="cust-res__price">
+              NT$ {numberWithCommas(middleNote.price)}
+            </td>
           </tr>
           <tr>
             <td>後調</td>
             <td>{baseNote.title}</td>
-            <td>NT$ {baseNote.price}</td>
+            <td className="cust-res__price">
+              NT$ {numberWithCommas(baseNote.price)}
+            </td>
           </tr>
         </tbody>
         <tfoot>
           <tr>
             <td></td>
             <td></td>
-            <td>NT$ {total}</td>
+            <td className="cust-res__price">NT$ {numberWithCommas(total)}</td>
           </tr>
         </tfoot>
       </table>
